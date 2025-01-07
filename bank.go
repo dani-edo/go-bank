f664ae5461dda3e59a45accbe7d01b79c781db39
dani_edo <dani.edogawa.17@gmail.com>
2025-01-07 14:45:11 +0700

    if statement execute by input

--- bank.go
+++ bank.go
@@ -3,10 +3,35 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance float64 = 1000
+
 	fmt.Println("Welcome to Go bank")
 	fmt.Println("What would you like to do?")
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	} else if choice == 2 {
+		var depositAmount float64
+		fmt.Println("How much would you like to deposit?")
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Balance updated! new amount: ", accountBalance)
+	} else if choice == 3 {
+		var withdrawalAmount float64
+		fmt.Println("How much would you like to withdraw?")
+		fmt.Scan(&withdrawalAmount)
+		accountBalance -= withdrawalAmount
+		fmt.Println("Balance updated! new amount: ", accountBalance)
+	} else {
+		fmt.Println("Goodbye!")
+	}
+
+	fmt.Println("Your choice is", choice)
 }
