7c030181f64bdb7d65a73e7217464bb4b2cee723
dani_edo <dani.edogawa.17@gmail.com>
2025-01-07 13:00:59 +0700

    go bank selection

---
+++ bank.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Go bank")
+	fmt.Println("What would you like to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+}
